62f06fe7379945e42952287a8c4732f6df607476
yseto <yseto@users.noreply.github.com>
2023-12-07 18:30:21 +0900

    fix for terraform-provider-aws v5.27.0 or later.

--- cloudwatch-logs-aggregator/rule/main.tf
+++ cloudwatch-logs-aggregator/rule/main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 resource "aws_cloudwatch_event_rule" "this" {
-  name                = var.rule_name
-  description         = "created by mackerel-monitoring-modules"
-  is_enabled          = var.is_enabled
+  name        = var.rule_name
+  description = "created by mackerel-monitoring-modules"
+  // TODO we will follow upstream?
+  state               = var.is_enabled
   schedule_expression = var.schedule_expression
 }
 
