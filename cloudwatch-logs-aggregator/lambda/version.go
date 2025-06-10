c51b442956b1e95e30ea3d374222582ebc860007
yseto <2216333+yseto@users.noreply.github.com>
2025-06-10 02:10:53 +0000

    chore: bump to v0.4.2

--- cloudwatch-logs-aggregator/lambda/version.go
+++ cloudwatch-logs-aggregator/lambda/version.go
@@ -3,4 +3,4 @@ package main
 // please use hard code version info.
 // because build a executable file before determine a tag.
 // this above will be almost resolved if stop use a this version info on useragent.
-const version = "0.4.1"
+const version = "0.4.2"
